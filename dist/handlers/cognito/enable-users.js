78819cf0c5ac3874e60c70e20dcad6bdaea2de36
Luis Oswaldo Garcia <luis.garcia.o@ab-inbev.com>
2025-12-17 17:33:41 -0600

    [FEAT] remove dist

--- dist/handlers/cognito/enable-users.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const controllers_1 = require("../../controllers");
-async function listUsers() {
-    const cognito = new controllers_1.CognitoAdmin();
-    await cognito.enableUsers();
-}
-listUsers();
